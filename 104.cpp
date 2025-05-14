e782c9e27fe57687742445054201ef8f7f10d8aa
Shuvo Islam Khan <shuvoislamkhan@gmail.com>
2025-05-14 19:52:06 +0600

    final

--- 104.cpp
+++ 104.cpp
@@ -2,36 +2,31 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-double f(double x, double a, double b, double c, double d, double e)
-{
+double f(double x, double a, double b, double c, double d, double e) {
     return a * pow(x, 4) + b * pow(x, 3) + c * pow(x, 2) + d * x + e;
 }
 // Define the derivative f'(x)
-double df(double x, double a, double b, double c, double d)
-{
+double df(double x, double a, double b, double c, double d) {
     return 4 * a * pow(x, 3) + 3 * b * pow(x, 2) + 2 * c * x + d;
 }
 // Newton-Raphson method to find root
 double newtonRaphson(double a, double b, double c, double d, double e, double x0, double tol =
-1e-6, int max_iter = 1000)
-{
-double x = x0;
-for (int i = 0; i < max_iter; i++)
-{
-    double fx = f(x, a, b, c, d, e);
-    double dfx = df(x, a, b, c, d);
-    if (fabs(dfx) < tol)
+1e-6, int max_iter = 1000) {
+    double x = x0;
+    for (int i = 0; i < max_iter; i++)
     {
-        // Avoid division by zero
-        cout << "Derivative too small, stopping iteration." << endl;
-        return x;
-    }
-    double x_new = x - fx / dfx;
-    if (fabs(x_new - x) < tol)
-    {
-        return x_new;
-    }
-    x = x_new;
+        double fx = f(x, a, b, c, d, e);
+        double dfx = df(x, a, b, c, d);
+        if (fabs(dfx) < tol) {
+            // Avoid division by zero
+            cout << "Derivative too small, stopping iteration." << endl;
+            return x;
+        }
+        double x_new = x - fx / dfx;
+        if (fabs(x_new - x) < tol) {
+            return x_new;
+        }
+        x = x_new;
     }
     cout << "Max iterations reached, solution may not be accurate." << endl;
     return x;
